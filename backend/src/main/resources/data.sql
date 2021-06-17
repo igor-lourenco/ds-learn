7ef3dddfdf577cd531de70399d3a57645de69289
igor-lourenco <igor.lourencosantos@gmail.com>
2021-06-17 17:26:53 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2018/02/27/10/49/training-3185170_1280.jpg', 'https://cdn.pixabay.com/photo/2017/05/16/15/08/courses-2318040_1280.jpg');
